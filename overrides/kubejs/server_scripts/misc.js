596482d55a39cfd14dad6b0d142621b269659b97
RoarkCats <roark007games@gmail.com>
2025-05-19 01:43:46 -0700

    - Added missing plank sawing recipes to the Mekanism Precision Sawmill

--- overrides/kubejs/server_scripts/misc.js
+++ overrides/kubejs/server_scripts/misc.js
@@ -4,4 +4,30 @@ ServerEvents.recipes(event => {
     event.forEachRecipe({ id: /productivebees:hives\/advanced_.*_beehive/ }, recipe => {
         event.replaceInput({ id: recipe.getId() }, '#forge:shears', 'minecraft:shears');
     });
+
+    // Add missing plank sawing recipes to mekanism Precision Sawmill
+    event.forEachRecipe({id:/create:cutting\/runtime_generated\/compat\/.*_planks$/}, recipe => {
+        const out = recipe.originalRecipeResult.id ;
+        const inp = Item.of(recipe.originalRecipeIngredients[0]).id ;
+
+        if (event.findRecipeIds({type:'mekanism:sawing', output: out}).length == 0) {
+            event.custom({
+                "type": "mekanism:sawing",
+                "input": {
+                  "ingredient": {
+                    "item": inp
+                  }
+                },
+                "mainOutput": {
+                  "count": 6,
+                  "item": out
+                },
+                "secondaryChance": 0.25,
+                "secondaryOutput": {
+                  "item": "mekanism:sawdust"
+                }
+            });
+        }
+
+    })
 })
