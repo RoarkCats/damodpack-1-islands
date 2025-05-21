699c801b3452e9af6ab8925400f4edaa15631e2b
RoarkCats <roark007games@gmail.com>
2025-05-20 19:18:45 -0700

    - Nerfed Sulfur Essence recipe output - Added variant Sulfur Essence recipes for all the types - Added `the_deep_void:sulfur_powder` to the tag `forge:dusts/sulfur`

--- overrides/kubejs/server_scripts/misc.js
+++ overrides/kubejs/server_scripts/misc.js
@@ -6,28 +6,95 @@ ServerEvents.recipes(event => {
     });
 
     // Add missing plank sawing recipes to mekanism Precision Sawmill
-    event.forEachRecipe({id:/create:cutting\/runtime_generated\/compat\/.*_planks$/}, recipe => {
-        const out = recipe.originalRecipeResult.id ;
-        const inp = Item.of(recipe.originalRecipeIngredients[0]).id ;
+    event.forEachRecipe({ id: /create:cutting\/runtime_generated\/compat\/.*_planks$/ }, recipe => {
+        const out = recipe.originalRecipeResult.id;
+        const inp = Item.of(recipe.originalRecipeIngredients[0]).id;
 
-        if (event.findRecipeIds({type:'mekanism:sawing', output: out}).length == 0) {
+        if (event.findRecipeIds({ type: 'mekanism:sawing', output: out }).length == 0) {
             event.custom({
                 "type": "mekanism:sawing",
                 "input": {
-                  "ingredient": {
-                    "item": inp
-                  }
+                    "ingredient": {
+                        "item": inp
+                    }
                 },
                 "mainOutput": {
-                  "count": 6,
-                  "item": out
+                    "count": 6,
+                    "item": out
                 },
                 "secondaryChance": 0.25,
                 "secondaryOutput": {
-                  "item": "mekanism:sawdust"
+                    "item": "mekanism:sawdust"
                 }
             });
         }
-
     })
+
+    // Sulfur recipes (Mystical Agriculture)
+    const sulf = 'mysticalagriculture:sulfur_essence';
+
+    event.remove({id: 'mysticalagriculture:essence/common/sulfur'}); // overwrite default
+    event.shaped('4x alexscaves:sulfur_dust', [
+        [sulf,sulf,sulf],
+        ['','',''],
+        ['','','']
+    ]).noMirror().noShrink();
+
+    event.shaped('4x the_deep_void:sulfur_powder', [
+        ['',sulf,''],
+        ['',sulf,''],
+        ['',sulf,'']
+    ]).noMirror().noShrink();
+
+    event.shaped('1x the_deep_void:sulfur', [
+        ['','',''],
+        ['',sulf,''],
+        [sulf,'',sulf]
+    ]).noMirror().noShrink();
+
+    event.shaped('4x mekanism:dust_sulfur', [
+        ['','',sulf],
+        ['',sulf,''],
+        [sulf,'','']
+    ]).noMirror().noShrink();
+
+    event.shaped('4x immersiveengineering:dust_sulfur', [
+        [sulf,'',''],
+        ['',sulf,''],
+        ['','',sulf]
+    ]).noMirror().noShrink();
+
+    // Sulfur (not dust) dissolving
+    event.custom({
+        "type": "alchemistry:dissolver",
+        "group": "alchemistry:dissolver",
+        "input": {
+            "count": 1,
+            "ingredient": {
+                "item": "the_deep_void:sulfur"
+            }
+        },
+        "output": {
+            "groups": [
+                {
+                    "probability": 100.0,
+                    "results": [
+                        {
+                            "item": "chemlib:sulfur",
+                            "count": 2
+                        }
+                    ]
+                }
+            ],
+            "rolls": 1,
+            "weighted": true
+        }
+    });
+
+})
+
+ServerEvents.tags('item', event => {
+
+    // Sulfur tag addition (The Deep Void)
+    event.add('forge:dusts/sulfur', 'the_deep_void:sulfur_powder');
 })
