fafbad10b0c314bb6e9be7d0e2488c6bf254995a
RoarkCats <roark007games@gmail.com>
2025-05-23 21:27:36 -0700

    - Fixed disabled inventory pet replace functionality for non-op players

--- overrides/kubejs/server_scripts/disabled_pet_replace.js
+++ overrides/kubejs/server_scripts/disabled_pet_replace.js
@@ -1,3 +1,7 @@
+function run_op_cmd(player, cmd) {
+    player.server.runCommandSilent(`execute as ${player.name.string} at @s run ${cmd}`);
+} // if you just do player.runCommand... then it'll fail if player's doesn't have perms
+
 PlayerEvents.inventoryChanged(event => {
     const {item, player/*, slot*/} = event;
 
@@ -7,9 +11,9 @@ PlayerEvents.inventoryChanged(event => {
         if (0 <= slot && slot <= 35) {slot = 'container.'+slot;}
         else if (slot == 40) {slot = 'weapon.offhand';}
         else {return;} // invalid slot or armor
-        player.runCommandSilent(`loot replace entity @s ${slot} loot dapack:replacement_pets`);
-        player.runCommandSilent('playsound block.fire.extinguish master @s ~ ~ ~ 0.5 1.2')
-        player.runCommandSilent('title @s actionbar {"text":"Inventory pet disabled","color":"red"}')
+        run_op_cmd(player, `loot replace entity @s ${slot} loot dapack:replacement_pets`);
+        run_op_cmd(player, 'playsound block.fire.extinguish master @s ~ ~ ~ 0.5 1.2');
+        run_op_cmd(player, 'title @s actionbar {"text":"Inventory pet disabled","color":"red"}');
         // player.inventory.setItem(slot,'stone') // setStackInSlot also good, player.inventory required tho!
     }
     // player.tell(`${event.server.lootData.getLootTable('gameplay/piglin_bartering').getRandomItems(???)}`) // not sure how to pass LootParams_
