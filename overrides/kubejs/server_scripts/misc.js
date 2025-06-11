07ca5a83227ca1042b79ab876e72cbf2a46de26d
RoarkCats <roark007games@gmail.com>
2025-06-11 15:21:04 -0700

    - Added vanilla Planks to the `_type_planks` tags for storage drawer/chest/barrel crafting - Added `_type_slab` tags for storage drawer/chest/barrel crafting (Fixed issue overlapping recipes)

--- overrides/kubejs/server_scripts/misc.js
+++ overrides/kubejs/server_scripts/misc.js
@@ -140,66 +140,38 @@ ServerEvents.recipes(event => {
 
     for (const type of ['oak','spruce','birch','jungle','acacia','dark_oak','mangrove','cherry','bamboo','crimson','warped']) {
         // Storage Drawers alt recipes (in place of every compat)
-        event.shaped(`1x storagedrawers:${type}_full_drawers_1`, [
-            'WWW',
-            ' C ',
-            'WWW',
-        ], {W:`#dapack:${type}_type_planks`, C:'#forge:chests/wooden'});
+        event.shaped(`1x storagedrawers:${type}_full_drawers_1`, ['WWW',' C ','WWW',], {W:`#dapack:${type}_type_planks`, C:'#forge:chests/wooden'});
 
-        event.shaped(`2x storagedrawers:${type}_full_drawers_2`, [
-            'WCW',
-            'WWW',
-            'WCW',
-        ], {W:`#dapack:${type}_type_planks`, C:'#forge:chests/wooden'});
+        event.shaped(`2x storagedrawers:${type}_full_drawers_2`, ['WCW','WWW','WCW',], {W:`#dapack:${type}_type_planks`, C:'#forge:chests/wooden'});
 
-        event.shaped(`4x storagedrawers:${type}_full_drawers_4`, [
-            'CWC',
-            'WWW',
-            'CWC',
-        ], {W:`#dapack:${type}_type_planks`, C:'#forge:chests/wooden'});
+        event.shaped(`4x storagedrawers:${type}_full_drawers_4`, ['CWC','WWW','CWC',], {W:`#dapack:${type}_type_planks`, C:'#forge:chests/wooden'});
 
-        event.shaped(`1x storagedrawers:${type}_half_drawers_1`, [
-            'WWW',
-            ' C ',
-            'WWW',
-        ], {W:`#dapack:${type}_type_planks`, C:'#forge:chests/wooden'});
+        event.shaped(`1x storagedrawers:${type}_half_drawers_1`, ['WWW',' C ','WWW',], {W:`#dapack:${type}_type_slab`, C:'#forge:chests/wooden'});
 
-        event.shaped(`2x storagedrawers:${type}_half_drawers_2`, [
-            'WCW',
-            'WWW',
-            'WCW',
-        ], {W:`#dapack:${type}_type_planks`, C:'#forge:chests/wooden'});
+        event.shaped(`2x storagedrawers:${type}_half_drawers_2`, ['WCW','WWW','WCW',], {W:`#dapack:${type}_type_slab`, C:'#forge:chests/wooden'});
 
-        event.shaped(`4x storagedrawers:${type}_half_drawers_4`, [
-            'CWC',
-            'WWW',
-            'CWC',
-        ], {W:`#dapack:${type}_type_planks`, C:'#forge:chests/wooden'});
+        event.shaped(`4x storagedrawers:${type}_half_drawers_4`, ['CWC','WWW','CWC',], {W:`#dapack:${type}_type_slab`, C:'#forge:chests/wooden'});
 
-        event.shaped(`4x storagedrawers:${type}_trim`, [
-            'WSW',
-            'SWS',
-            'WSW',
-        ], {W:`#dapack:${type}_type_planks`, S:'#forge:rods/wooden'});
+        event.shaped(`4x storagedrawers:${type}_trim`, ['WSW','SWS','WSW',], {W:`#dapack:${type}_type_planks`, S:'#forge:rods/wooden'});
         
         // Sophisticated Storage alt recipes (for continuity)
         event.shaped(Item.of('sophisticatedstorage:chest', {woodType: type}),
         ['WWW','WTW','WWW'], {W:`#dapack:${type}_type_planks`, T:'minecraft:redstone_torch'});
 
         event.shaped(Item.of('sophisticatedstorage:barrel', {woodType: type}),
-        ['WSW','WTW','WSW'], {W:`#dapack:${type}_type_planks`, S:'#minecraft:wooden_slabs', T:'minecraft:redstone_torch'});
+        ['WSW','WTW','WSW'], {W:`#dapack:${type}_type_planks`, S:`#dapack:${type}_type_slab`, T:'minecraft:redstone_torch'});
 
         event.shaped(Item.of('sophisticatedstorage:limited_barrel_1', {woodType: type}),
-        ['WSW','WTW','WWW'], {W:`#dapack:${type}_type_planks`, S:'#minecraft:wooden_slabs', T:'minecraft:redstone_torch'});
+        ['WSW','WTW','WWW'], {W:`#dapack:${type}_type_planks`, S:`#dapack:${type}_type_slab`, T:'minecraft:redstone_torch'});
 
         event.shaped(Item.of('sophisticatedstorage:limited_barrel_2', {woodType: type}),
-        ['WWW','STS','WWW'], {W:`#dapack:${type}_type_planks`, S:'#minecraft:wooden_slabs', T:'minecraft:redstone_torch'});
+        ['WWW','STS','WWW'], {W:`#dapack:${type}_type_planks`, S:`#dapack:${type}_type_slab`, T:'minecraft:redstone_torch'});
 
         event.shaped(Item.of('sophisticatedstorage:limited_barrel_3', {woodType: type}),
-        ['WSW','WTW','SWS'], {W:`#dapack:${type}_type_planks`, S:'#minecraft:wooden_slabs', T:'minecraft:redstone_torch'});
+        ['WSW','WTW','SWS'], {W:`#dapack:${type}_type_planks`, S:`#dapack:${type}_type_slab`, T:'minecraft:redstone_torch'});
 
         event.shaped(Item.of('sophisticatedstorage:limited_barrel_4', {woodType: type}),
-        ['SWS','WTW','SWS'], {W:`#dapack:${type}_type_planks`, S:'#minecraft:wooden_slabs', T:'minecraft:redstone_torch'});
+        ['SWS','WTW','SWS'], {W:`#dapack:${type}_type_planks`, S:`#dapack:${type}_type_slab`, T:'minecraft:redstone_torch'});
     }
     event.remove({id:/sophisticatedstorage:generic_/});
 
@@ -290,90 +262,103 @@ ServerEvents.tags('item', event => {
     event.add('inventorypets:raw_meats', ['#forge:rawmeats']); // this technically has fish too but I don't really care 
 
     // Wood tags for Storage Drawers / Sophisticated Chests
-    event.add('dapack:oak_type_planks', [
-        "twilightforest:twilight_oak_planks",
-        "twilightforest:transformation_planks",
-        "regions_unexplored:alpha_planks",
-        "regions_unexplored:maple_planks",
-    ]);
-    event.add('dapack:spruce_type_planks', [
-        "upgrade_aquatic:driftwood_planks",
-        "twilightforest:canopy_planks",
-        "twilightforest:time_planks",
-        "upgrade_aquatic:river_planks",
-        "regions_unexplored:dead_planks",
-    ]);
-    event.add('dapack:birch_type_planks', [
-        "ad_astra:strophar_planks",
-        "deep_aether:sunroot_planks",
-        "twilightforest:mangrove_planks",
-        "twilightforest:mining_planks",
-        "regions_unexplored:palm_planks",
-        "regions_unexplored:willow_planks",
-    ]);
-    event.add('dapack:jungle_type_planks', [
-        "deep_aether:conberry_planks",
-        "twilightforest:dark_planks",
-        "alexscaves:pewen_planks",
-        "regions_unexplored:baobab_planks",
-        "regions_unexplored:joshua_planks",
-        "regions_unexplored:kapok_planks",
-        "regions_unexplored:larch_planks",
-        "regions_unexplored:pine_planks",
-        "regions_unexplored:socotra_planks",
-    ]);
-    event.add('dapack:acacia_type_planks', [
-        "unusualprehistory:ginkgo_planks",
-        "applewood:apple_planks",
-        "regions_unexplored:eucalyptus_planks",
-        "regions_unexplored:redwood_planks",
-    ]);
-    event.add('dapack:dark_oak_type_planks', [
-        "the_deep_void:rotten_planks",
-        "iceandfire:dreadwood_planks",
-        "vampirism:dark_spruce_planks",
-        "twilightforest:sorting_planks",
-        "alexscaves:thornwood_planks",
-        "regions_unexplored:blackwood_planks",
-        "regions_unexplored:brimwood_planks",
-    ]);
-    event.add('dapack:mangrove_type_planks', [
-        "botania:livingwood_planks",
-        "unusualprehistory:foxxi_planks",
-        "vinery:dark_cherry_planks",
-        "vampirism:cursed_spruce_planks",
-        "regions_unexplored:magnolia_planks",
-    ]);
-    event.add('dapack:cherry_type_planks', [
-        "totemic:cedar_planks",
-        "ad_astra:glacian_planks",
-        "unusualprehistory:dryo_planks",
-        "deep_aether:roseroot_planks",
-        "werewolves:jacaranda_planks",
-    ]);
-    event.add('dapack:bamboo_type_planks', [
-        "beachparty:palm_planks",
-        "regions_unexplored:cypress_planks",
-        "aether:skyroot_planks",
-        "regions_unexplored:yellow_bioshroom_planks",
-    ]);
-    event.add('dapack:crimson_type_planks', [
-        "cataclysm:chorus_planks",
-        "deep_aether:yagroot_planks",
-        "deep_aether:cruderoot_planks",
-        "regions_unexplored:mauve_planks",
-        "regions_unexplored:pink_bioshroom_planks",
-    ]);
-    event.add('dapack:warped_type_planks', [
-        "botania:dreamwood_planks",
-        "integrateddynamics:menril_planks",
-        "ars_nouveau:archwood_planks",
-        "ad_astra:aeronos_planks",
-        "werewolves:magic_planks",
-        "regions_unexplored:blue_bioshroom_planks",
-        "regions_unexplored:cobalt_planks",
-        "regions_unexplored:green_bioshroom_planks",
-    ]);
+    for (const planks_slab of ['planks', 'slab']) {
+        event.add(`dapack:oak_type_${planks_slab}`, [
+            `minecraft:oak_${planks_slab}`,
+            `twilightforest:twilight_oak_${planks_slab}`,
+            `twilightforest:transformation_${planks_slab}`,
+            `regions_unexplored:alpha_${planks_slab}`,
+            `regions_unexplored:maple_${planks_slab}`,
+        ]);
+        event.add(`dapack:spruce_type_${planks_slab}`, [
+            `minecraft:spruce_${planks_slab}`,
+            `upgrade_aquatic:driftwood_${planks_slab}`,
+            `twilightforest:canopy_${planks_slab}`,
+            `twilightforest:time_${planks_slab}`,
+            `upgrade_aquatic:river_${planks_slab}`,
+            `regions_unexplored:dead_${planks_slab}`,
+        ]);
+        event.add(`dapack:birch_type_${planks_slab}`, [
+            `minecraft:birch_${planks_slab}`,
+            `ad_astra:strophar_${planks_slab}`,
+            `deep_aether:sunroot_${planks_slab}`,
+            `twilightforest:mangrove_${planks_slab}`,
+            `twilightforest:mining_${planks_slab}`,
+            `regions_unexplored:palm_${planks_slab}`,
+            `regions_unexplored:willow_${planks_slab}`,
+        ]);
+        event.add(`dapack:jungle_type_${planks_slab}`, [
+            `minecraft:jungle_${planks_slab}`,
+            `deep_aether:conberry_${planks_slab}`,
+            `twilightforest:dark_${planks_slab}`,
+            `alexscaves:pewen_${planks_slab}`,
+            `regions_unexplored:baobab_${planks_slab}`,
+            `regions_unexplored:joshua_${planks_slab}`,
+            `regions_unexplored:kapok_${planks_slab}`,
+            `regions_unexplored:larch_${planks_slab}`,
+            `regions_unexplored:pine_${planks_slab}`,
+            `regions_unexplored:socotra_${planks_slab}`,
+        ]);
+        event.add(`dapack:acacia_type_${planks_slab}`, [
+            `minecraft:acacia_${planks_slab}`,
+            `unusualprehistory:ginkgo_${planks_slab}`,
+            `applewood:apple_${planks_slab}`,
+            `regions_unexplored:eucalyptus_${planks_slab}`,
+            `regions_unexplored:redwood_${planks_slab}`,
+        ]);
+        event.add(`dapack:dark_oak_type_${planks_slab}`, [
+            `minecraft:dark_oak_${planks_slab}`,
+            `the_deep_void:rotten_${planks_slab}`,
+            `iceandfire:dreadwood_${planks_slab}`,
+            `vampirism:dark_spruce_${planks_slab}`,
+            `twilightforest:sorting_${planks_slab}`,
+            `alexscaves:thornwood_${planks_slab}`,
+            `regions_unexplored:blackwood_${planks_slab}`,
+            `regions_unexplored:brimwood_${planks_slab}`,
+        ]);
+        event.add(`dapack:mangrove_type_${planks_slab}`, [
+            `minecraft:mangrove_${planks_slab}`,
+            `botania:livingwood_${planks_slab}`,
+            `unusualprehistory:foxxi_${planks_slab}`,
+            `vinery:dark_cherry_${planks_slab}`,
+            `vampirism:cursed_spruce_${planks_slab}`,
+            `regions_unexplored:magnolia_${planks_slab}`,
+        ]);
+        event.add(`dapack:cherry_type_${planks_slab}`, [
+            `minecraft:cherry_${planks_slab}`,
+            `totemic:cedar_${planks_slab}`,
+            `ad_astra:glacian_${planks_slab}`,
+            `unusualprehistory:dryo_${planks_slab}`,
+            `deep_aether:roseroot_${planks_slab}`,
+            `werewolves:jacaranda_${planks_slab}`,
+        ]);
+        event.add(`dapack:bamboo_type_${planks_slab}`, [
+            `minecraft:bamboo_${planks_slab}`,
+            `beachparty:palm_${planks_slab}`,
+            `regions_unexplored:cypress_${planks_slab}`,
+            `aether:skyroot_${planks_slab}`,
+            `regions_unexplored:yellow_bioshroom_${planks_slab}`,
+        ]);
+        event.add(`dapack:crimson_type_${planks_slab}`, [
+            `minecraft:crimson_${planks_slab}`,
+            `cataclysm:chorus_${planks_slab}`,
+            `deep_aether:yagroot_${planks_slab}`,
+            `deep_aether:cruderoot_${planks_slab}`,
+            `regions_unexplored:mauve_${planks_slab}`,
+            `regions_unexplored:pink_bioshroom_${planks_slab}`,
+        ]);
+        event.add(`dapack:warped_type_${planks_slab}`, [
+            `minecraft:warped_${planks_slab}`,
+            `botania:dreamwood_${planks_slab}`,
+            `integrateddynamics:menril_${planks_slab}`,
+            `ars_nouveau:archwood_${planks_slab}`,
+            `ad_astra:aeronos_${planks_slab}`,
+            `werewolves:magic_${planks_slab}`,
+            `regions_unexplored:blue_bioshroom_${planks_slab}`,
+            `regions_unexplored:cobalt_${planks_slab}`,
+            `regions_unexplored:green_bioshroom_${planks_slab}`,
+        ]);
+    }
 })
 
 ServerEvents.tags('fluid', event => {
